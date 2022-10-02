7c78e108624eed89efd4e2eca15dc7b1a1230380
karmugilan7399 <karmugilan7399@gmail.com>
2022-10-02 14:44:23 +0530

    Task

---
+++ src/org/collection/Test1.java
@@ -0,0 +1,20 @@
+package org.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+public static void main(String[] args) {
+List<String> l=new ArrayList<>();
+l.add("sanjsh");
+l.add("novfeel");
+l.add("java");
+l.add("Selenium");
+l.add("Framework");
+//System.out.println(l);
+for (String s : l) {
+	System.out.println(s);
+}
+
+}
+}
